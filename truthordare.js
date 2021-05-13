9fdccce044be33d677f9e8af97e74eee551087ff
PersonalWillR <83924795+PersonalWillR@users.noreply.github.com>
2021-05-12 22:47:46 -0400

    Add files via upload

--- truthordare.js
+++ truthordare.js
@@ -1,18 +1,27 @@
 var truths = []
 var dares = []
 
-const logFileText = async file => {
-    const response = await fetch(file, {
-		
-		mode: 'no-cors'
-	
-	})
-    const text = await response.text()
-    return text
+function loadFile(file, onLoad) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET",file,true);
+	rawFile.onreadystatechange = function() {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				var allText = rawFile.responseText;
+				onLoad(allText)
+			}
+		}
+	}
+	rawFile.send()
 }
 
-truths = logFileText('truths.txt').split("\n")
-dares = logFileText('dares.txt').split("\n")
+var truthsText = loadFile('https://personalwillr.github.io/truths.txt', (text)=>{
+	truths = text.split("\n")
+	console.log("LOADED")
+})
+var daresText = loadFile('https://personalwillr.github.io/dares.txt', (text)=>{
+	dares = text.split("\n")
+})
 
 function randomFromList(list) {
 	return list[parseInt(Math.random() * list.length)]
@@ -20,9 +29,11 @@ function randomFromList(list) {
 }
 
 document.getElementById("truth").addEventListener("click", (e) => {
-	document.getElementById("text").value = randomFromList(truths);
+	let val = randomFromList(truths);
+	console.log(truths);
+	document.getElementById("text").innerHTML = val;
 })
 
 document.getElementById("dare").addEventListener("click", (e) => {
-	document.getElementById("text").value = randomFromList(dares);
+	document.getElementById("text").innerHTML = randomFromList(dares);
 })
